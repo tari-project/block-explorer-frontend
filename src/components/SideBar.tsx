dcd72f4974eb3147956f17c52678ef89775b4967
Bojan Čoka <bojan.coka@pbssolutions.net>
2020-05-11 11:11:28 +0200

    Add initial SVG assets

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { ReactComponent as Blocks } from "../assets/blocks.svg";
+import { ReactComponent as Bubbles } from "../assets/bubbles.svg";
+import { ReactComponent as Cube } from "../assets/cube.svg";
 import "./SideBar.css";
 
 export default function SideBar() {
-  return <div className="SideBar"></div>;
+  return (
+    <div className="SideBar">
+      <div className="SideBar-navigationItem">
+        <Blocks />
+      </div>
+      <div className="SideBar-navigationItem">
+        <Cube />
+      </div>
+      <div className="SideBar-navigationItem">
+        <Bubbles />
+      </div>
+    </div>
+  );
 }
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { ReactComponent as Logo } from "../assets/logo.svg";
 import "./TopBar.css";
 
 export default function TopBar() {
   return (
     <div className="TopBar">
-      <div className="TopBar-logoContainer"></div>
+      <div className="TopBar-logoContainer">
+        <Logo fill="#9330ff" />
+      </div>
       <div className="TopBar-searchContainer"></div>
     </div>
   );
