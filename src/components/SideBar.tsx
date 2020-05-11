c94c78dc0edba31bf7c441d18c5f68b4da215365
Bojan Čoka <bojan.coka@pbssolutions.net>
2020-05-11 10:49:05 +0200

    Lay out the main view

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./SideBar.css";
+
+export default function SideBar() {
+  return <div className="SideBar"></div>;
+}
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./TopBar.css";
+
+export default function TopBar() {
+  return (
+    <div className="TopBar">
+      <div className="TopBar-logoContainer"></div>
+      <div className="TopBar-searchContainer"></div>
+    </div>
+  );
+}
