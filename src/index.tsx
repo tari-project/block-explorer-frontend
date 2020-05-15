848c68602044abc25fbb5492b1e0c1df39d2921d
Bojan Čoka <bojan.coka@pbssolutions.net>
2020-05-15 14:44:43 +0200

    Set up hot reload

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+function render(AppComponent: () => JSX.Element) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <AppComponent />
+    </React.StrictMode>,
+    rootElement
+  );
+}
+
+render(App);
+
+if (process.env.NODE_ENV === "development" && module.hot) {
+  module.hot.accept("./App", () => {
+    const NextApp = require("./App").default;
+    render(NextApp);
+  });
+}
