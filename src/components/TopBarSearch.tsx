1470ba2bdf47c5ddb2d961e01828a209655bafd2
Shannon Tenner <shanimal@tari.com>
2020-05-22 14:12:02 +0200

    Search bar

---
+++ src/components/TopBarSearch.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ReactComponent as Search } from '../assets/search.svg';
+
+import './TopBarSearch.css';
+
+export default function TopBarSearch() {
+    return (
+        <div className="TopBar-searchBar">
+            <input type="text" placeholder="Search for a specific block or transaction"/>
+            <Search className="searchBarIcon"/>
+        </div>
+    );
+}
