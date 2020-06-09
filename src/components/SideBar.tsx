8367cbe3f9b69c2ce4a00969ea6cdef6c7538053
Shan <47271333+shanimal08@users.noreply.github.com>
2020-06-09 14:18:52 +0200

    Commented out menu icons for now (#44)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,15 +7,15 @@ import "./SideBar.css";
 export default function SideBar() {
   return (
     <div className="SideBar">
-      <div className="SideBar-navigationItem">
-        <Blocks />
-      </div>
-      <div className="SideBar-navigationItem">
-        <Cube />
-      </div>
-      <div className="SideBar-navigationItem">
-        <Bubbles />
-      </div>
+      {/*<div className="SideBar-navigationItem">*/}
+      {/*  <Blocks />*/}
+      {/*</div>*/}
+      {/*<div className="SideBar-navigationItem">*/}
+      {/*  <Cube />*/}
+      {/*</div>*/}
+      {/*<div className="SideBar-navigationItem">*/}
+      {/*  <Bubbles />*/}
+      {/*</div>*/}
     </div>
   );
 }
