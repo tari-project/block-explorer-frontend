4fa79424d9bd283c064e1e2ad6a6dde6f644fe3e
Shan <47271333+shanimal08@users.noreply.github.com>
2020-06-12 11:55:25 +0200

    Removed placeholder graph in Latest Block Cards (#61)

--- src/components/BlockCard.tsx
+++ src/components/BlockCard.tsx
@@ -19,16 +19,6 @@ export default function BlockCard({ block }: { block: any }) {
     return (
         <div key={height} className="BlockCard slideIn">
             <Header blockHeight={heightStr} date={date} />
-            <div className="BlockCard-chart">
-                <BlockChart />
-            </div>
-            <div className="BlockCard-ticks">
-                <div>0:00</div>
-                <div>0:30</div>
-                <div>1:00</div>
-                <div>1:30</div>
-                <div>1:48</div>
-            </div>
             <div className="BlockCard-stats">
                 <StatBox color="yellow" label="# of Transactions" value={kernels.length} />
                 <StatBox color="blue" label="Mining Time" value={miningTime} />
