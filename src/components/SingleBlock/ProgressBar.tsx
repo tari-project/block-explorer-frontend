b22156ac7595545d411dbfe92083375b3fab0797
Yentl <yenty@tari.com>
2020-06-15 16:57:11 +0200

    Updated label for progress bar

--- src/components/SingleBlock/ProgressBar.tsx
+++ src/components/SingleBlock/ProgressBar.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function ProgressBar({ weight, maxWeight }: Props) {
     return (
         <div className="ProgressBarContainer">
-            <h3><strong>Weight: </strong> {weight}</h3>
+            <h3><strong>Weight: </strong> {weight} / {maxWeight}</h3>
             <progress value={weight} max={maxWeight} />
         </div>
     );
