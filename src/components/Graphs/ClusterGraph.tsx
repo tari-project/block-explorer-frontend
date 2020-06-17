3282ec85bbe7868745ae2a93b70b58b084cef4cf
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-06-17 12:19:24 +0200

    Added loading and error states to Single block view
    Added useEffect for ClusterGraph for re-rendering on link change

--- src/components/Graphs/ClusterGraph.tsx
+++ src/components/Graphs/ClusterGraph.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as d3 from 'd3';
 import './ClusterGraph.css';
 import { ClusterPoint } from '../../types/SingleBlockGraph';
@@ -10,96 +10,100 @@ interface Props {
 }
 
 export default function ClusterGraph({ width, height, data }: Props) {
-    const maxSize = Math.max(...data.map((o) => o.size));
-    const radiusScale = d3.scaleSqrt().domain([1, maxSize]).range([0, 10]);
+    useEffect(() => {
+        const maxSize = Math.max(...data.map((o) => o.size));
+        const radiusScale = d3.scaleSqrt().domain([1, maxSize]).range([0, 10]);
 
-    const forceX = d3
-        .forceX(function (d: any) {
-            if (d.group === 'inputs') {
-                return width / 2;
-            } else if (d.group === 'kernels') {
-                return width / 2 + 50;
-            } else {
-                return width / 2 + 130;
-            }
-        })
-        .strength(0.1);
-    const simulation = d3
-        .forceSimulation()
-        .force('x', forceX)
-        .force('y', d3.forceY(height / 2).strength(0.1))
-        .force(
-            'collide',
-            d3.forceCollide(function (d: any) {
-                return radiusScale(d.size) * 4 + 1;
+        const forceX = d3
+            .forceX(function (d: any) {
+                if (d.group === 'inputs') {
+                    return width / 2;
+                } else if (d.group === 'kernels') {
+                    return width / 2 + 50;
+                } else {
+                    return width / 2 + 130;
+                }
             })
-        );
+            .strength(0.1);
+        const simulation = d3
+            .forceSimulation()
+            .force('x', forceX)
+            .force('y', d3.forceY(height / 2).strength(0.1))
+            .force(
+                'collide',
+                d3
+                    .forceCollide(function (d: any) {
+                        return radiusScale(d.size) * 4 + 1;
+                    })
+                    .iterations(1)
+            );
 
-    function ready(dataPoints) {
-        const svg = d3
-            .select('#chart')
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .attr('preserveAspectRatio', 'xMidYMid meet')
-            .append('g')
-            .attr('transform', 'translate(0, 0)');
+        function ready(dataPoints) {
+            const svg = d3
+                .select('#chart')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height)
+                .attr('preserveAspectRatio', 'xMidYMid meet')
+                .append('g')
+                .attr('transform', 'translate(0, 0)');
 
-        const tooltip = d3
-            .select('#chart')
-            .append('div')
-            .style('opacity', 0.5)
-            .attr('class', 'tooltip')
-            .style('background-color', 'black')
-            .style('border-radius', '5px')
-            .style('padding', '10px')
-            .style('color', 'white');
-        const showTooltip = function (this: any, d) {
-            tooltip.transition().duration(200);
-            tooltip
-                .style('opacity', 1)
-                .html(d.tooltip)
-                .style('left', d3.mouse(this)[0] + 30 + 'px')
-                .style('top', d3.mouse(this)[1] + 30 + 'px');
-        };
-        const moveTooltip = function (this: any, d) {
-            tooltip.style('left', d3.mouse(this)[0] + 30 + 'px').style('top', d3.mouse(this)[1] + 30 + 'px');
-        };
-        const hideTooltip = function (d) {
-            tooltip.transition().duration(200).style('opacity', 0);
-        };
+            const tooltip = d3
+                .select('#chart')
+                .append('div')
+                .style('opacity', 0)
+                .attr('class', 'tooltip')
+                .style('background-color', 'black')
+                .style('border-radius', '5px')
+                .style('padding', '10px')
+                .style('color', 'white');
+            const showTooltip = function (this: any, d) {
+                tooltip.transition().duration(200);
+                tooltip
+                    .style('opacity', 1)
+                    .html(d.tooltip)
+                    .style('left', d3.mouse(this)[0] + 30 + 'px')
+                    .style('top', d3.mouse(this)[1] + 30 + 'px');
+            };
+            const moveTooltip = function (this: any, d) {
+                tooltip.style('left', d3.mouse(this)[0] + 30 + 'px').style('top', d3.mouse(this)[1] + 30 + 'px');
+            };
+            const hideTooltip = function (d) {
+                tooltip.transition().duration(200).style('opacity', 0);
+            };
 
-        const circles = svg
-            .selectAll('bubble')
-            .data(dataPoints)
-            .enter()
-            .append('circle')
-            .attr('class', 'bubble')
-            .attr('r', function (d: any) {
-                return radiusScale(d.size) * 4;
-            })
-            .attr('fill', function (d: any) {
-                return d.color;
-            })
-            .attr('cx', 100)
-            .attr('cy', 100)
-            .on('mouseover', showTooltip)
-            .on('mousemove', moveTooltip)
-            .on('mouseleave', hideTooltip);
-
-        function ticked() {
-            circles
-                .attr('cx', function (d: any) {
-                    return d.x;
+            const circles = svg
+                .selectAll('bubble')
+                .data(dataPoints)
+                .enter()
+                .append('circle')
+                .attr('class', 'bubble')
+                .attr('r', function (d: any) {
+                    return radiusScale(d.size) * 4;
                 })
-                .attr('cy', function (d: any) {
-                    return d.y;
-                });
-        }
-        simulation.nodes(dataPoints).on('tick', ticked);
-    }
+                .attr('fill', function (d: any) {
+                    return d.color;
+                })
+                .attr('cx', 100)
+                .attr('cy', 100)
+                .on('mouseover', showTooltip)
+                .on('mousemove', moveTooltip)
+                .on('mouseleave', hideTooltip);
 
-    data && data.length > 0 && ready(data);
+            function ticked() {
+                circles
+                    .attr('cx', function (d: any) {
+                        return d.x;
+                    })
+                    .attr('cy', function (d: any) {
+                        return d.y;
+                    });
+            }
+            simulation.nodes(dataPoints).on('tick', ticked);
+        }
+
+        data && data.length > 0 && ready(data);
+    }, [data, width, height]);
 
     return <div id="chart" />;
 }
--- src/components/SingleBlock.tsx
+++ src/components/SingleBlock.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import React, { useEffect, useState } from 'react';
 import './SingleBlock.css';
 import { useParams } from 'react-router-dom';
@@ -10,33 +11,89 @@ import ClusterGraph from './Graphs/ClusterGraph';
 import { Block, Body, Header, Pow } from '../types/Blocks';
 import { Link } from 'react-router-dom';
 import { ClusterPoint } from '../types/SingleBlockGraph';
-
+import { ReactComponent as LoadingBars } from '../assets/bars.svg';
 interface Props {
     constants: Constants;
 }
 
+interface Status {
+    status: string;
+    message: string;
+}
+
 function SingleBlock({ constants }: Props) {
     const { id } = useParams();
-
     const [singleBlock, setSingleBlock] = useState({} as Block);
     const [blockHeader, setblockHeader] = useState({} as Header);
     const [blockPow, setblockPow] = useState({} as Pow);
-    const [blockBody, setblockBody] = useState({} as Body);
-
+    const [status, setStatus] = useState({ status: 'loading', message: '' } as Status);
+    const [clusterData, setClusterData] = useState([] as ClusterPoint[]);
     useEffect(() => {
-        try {
-            id &&
-                fetchSingleBlock(id).then((block) => {
-                    if (block && block.block) {
-                        setSingleBlock(block.block);
-                        setblockHeader(block.block.header as Header);
-                        setblockPow(block.block.header.pow as Pow);
-                        setblockBody(block.block.body as Body);
-                    }
-                });
-        } catch (e) {
-            console.error(e);
+        if (!id) {
+            setStatus({
+                status: 'error',
+                message: 'Invalid block height or hash'
+            });
+            return;
         }
+        setStatus({
+            status: 'loading',
+            message: ''
+        });
+        fetchSingleBlock(id)
+            .then((block) => {
+                if (block && block.block) {
+                    setSingleBlock(block.block);
+                    setblockHeader(block.block.header as Header);
+                    setblockPow(block.block.header.pow as Pow);
+                    const blockBody = block.block.body as Body;
+                    const { inputs = [], kernels = [], outputs = [] } = blockBody;
+                    let singleBlockDataArray: ClusterPoint[] = [];
+                    singleBlockDataArray = singleBlockDataArray.concat(
+                        inputs.map((i) => ({
+                            group: 'inputs',
+                            size: inputs.length,
+                            color: '#F97C0C',
+                            tooltip: `Input<br/>${i.commitment.slice(0, 10)}`
+                        }))
+                    );
+
+                    singleBlockDataArray = singleBlockDataArray.concat(
+                        kernels.map((i) => ({
+                            group: 'kernels',
+                            size: kernels.length,
+                            color: '#FB576D',
+                            tooltip: `Kernel<br/>${i.excess.slice(0, 10)}`
+                        }))
+                    );
+
+                    singleBlockDataArray = singleBlockDataArray.concat(
+                        outputs.map((i) => ({
+                            group: 'outputs',
+                            size: outputs.length,
+                            color: '#2274AF',
+                            tooltip: `Output<br/>${i.commitment.slice(0, 10)}`
+                        }))
+                    );
+                    setClusterData(singleBlockDataArray);
+                    setStatus({
+                        status: 'complete',
+                        message: ''
+                    });
+                } else {
+                    setStatus({
+                        status: 'error',
+                        message: 'There was an error fetching the block'
+                    });
+                }
+            })
+            .catch((e) => {
+                console.error(e);
+                setStatus({
+                    status: 'error',
+                    message: 'Block not found'
+                });
+            });
     }, [id]);
 
     const [constantsData, setConstantsData] = useState({} as Constants);
@@ -46,35 +103,6 @@ function SingleBlock({ constants }: Props) {
 
     const { hash, prev_hash, nonce, total_kernel_offset, version, timestamp } = blockHeader;
     const { accumulated_monero_difficulty, accumulated_blake_difficulty } = blockPow;
-    const { inputs = [], kernels = [], outputs = [] } = blockBody;
-
-    let singleBlockDataArray: ClusterPoint[] = [];
-    singleBlockDataArray = singleBlockDataArray.concat(
-        inputs.map((i) => ({
-            group: 'inputs',
-            size: inputs.length,
-            color: '#F97C0C',
-            tooltip: `Input<br/>${i.commitment.slice(0, 10)}`
-        }))
-    );
-
-    singleBlockDataArray = singleBlockDataArray.concat(
-        kernels.map((i) => ({
-            group: 'kernels',
-            size: kernels.length,
-            color: '#FB576D',
-            tooltip: `Kernel<br/>${i.excess.slice(0, 10)}`
-        }))
-    );
-
-    singleBlockDataArray = singleBlockDataArray.concat(
-        outputs.map((i) => ({
-            group: 'outputs',
-            size: outputs.length,
-            color: '#2274AF',
-            tooltip: `Output<br/>${i.commitment.slice(0, 10)}`
-        }))
-    );
 
     const date = timestamp && new Date(timestamp.seconds * 1000).toLocaleString();
     const { _weight } = singleBlock;
@@ -82,10 +110,10 @@ function SingleBlock({ constants }: Props) {
 
     return (
         <div className="SingleBlock">
-            {hash ? (
+            {clusterData.length && status.status === 'complete' ? (
                 <div>
                     <SingleBlockViewHeader title="Block Data" />
-                    <ClusterGraph data={singleBlockDataArray} width={1000} height={400} />
+                    <ClusterGraph data={clusterData} width={1000} height={400} />
                     <ProgressBar weight={_weight} maxWeight={max_block_transaction_weight} />
                     <h1>Mining Details</h1>
                     <StatRow label="Timestamp" value={date} />
@@ -102,8 +130,12 @@ function SingleBlock({ constants }: Props) {
                     <StatRow label="Total Kernel Offset" value={total_kernel_offset} />
                     <StatRow label="Version" value={version} />
                 </div>
+            ) : status.status === 'loading' ? (
+                <LoadingBars fill={'#000'} />
             ) : (
-                <h1 className="noBlockFound">No block found.</h1>
+                <h1 className="noBlockFound">
+                    {status.message} <Link to={'/'}>Go Back</Link>
+                </h1>
             )}
         </div>
     );
