527a9579d8aac820d03f96a26926af277c211ae5
Shannon Tenner <shanimal@tari.com>
2020-06-23 14:37:36 +0200

    Click scroll WIP

--- src/components/LatestBlocks.tsx
+++ src/components/LatestBlocks.tsx
@@ -1,27 +1,40 @@
 import BlockCard from './BlockCard';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import { BlocksEntity } from '../types/Blocks';
 import { ReactComponent as RightCaret } from '../assets/right-caret.svg';
 import './BlockCard.css';
-import ScrollContainer from 'react-indiana-drag-scroll';
 
 function LatestBlocks({ blocks }: { blocks: BlocksEntity[] }) {
+    const container = useRef<HTMLDivElement>(null);
+    const containerCurrent = container.current;
     const [latestBlocks, setLatestBlocks] = useState([] as BlocksEntity[]);
     useEffect(() => {
         blocks.sort((a, b) => b.block.header.height - a.block.header.height);
         setLatestBlocks(blocks as BlocksEntity[]);
     }, [blocks]);
 
+    const [leftVal, setLeftVal] = useState(0);
+
+    function scrollRight() {
+        if (containerCurrent) {
+            setLeftVal(leftVal + containerCurrent.clientWidth);
+            containerCurrent.scroll({ top: 0, left: leftVal, behavior: 'smooth' });
+            console.log(leftVal);
+        }
+    }
+
     return (
         <div className="latestBlocksAll">
-            <ScrollContainer className="latestBlocksContainer" hideScrollbars={false} vertical={false}>
+            <div ref={container} className="latestBlocksContainer">
                 {latestBlocks.map((block, i) => (
                     <BlockCard key={i} block={block} />
                 ))}
-            </ScrollContainer>
-            <RightCaret className="rightCaret" />
-            <RightCaret className="rightCaret small" />
+            </div>
+            <div className="clickScroll" onClick={scrollRight}>
+                <RightCaret className="rightCaret" />
+                <RightCaret className="rightCaret small" />
+            </div>
         </div>
     );
 }
