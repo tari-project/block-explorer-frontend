6624f336a1570ee0c66b147de25a735fd1d3d260
Shannon Tenner <shanimal@tari.com>
2020-06-23 15:26:59 +0200

    Click scroll (right) WITH drag scroll

--- src/components/LatestBlocks.tsx
+++ src/components/LatestBlocks.tsx
@@ -4,34 +4,50 @@ import { connect } from 'react-redux';
 import { BlocksEntity } from '../types/Blocks';
 import { ReactComponent as RightCaret } from '../assets/right-caret.svg';
 import './BlockCard.css';
+import ScrollContainer from 'react-indiana-drag-scroll';
 
 function LatestBlocks({ blocks }: { blocks: BlocksEntity[] }) {
-    const container = useRef<HTMLDivElement>(null);
-    const containerCurrent = container.current;
     const [latestBlocks, setLatestBlocks] = useState([] as BlocksEntity[]);
     useEffect(() => {
         blocks.sort((a, b) => b.block.header.height - a.block.header.height);
         setLatestBlocks(blocks as BlocksEntity[]);
     }, [blocks]);
 
-    const [leftVal, setLeftVal] = useState(0);
+    const [leftVal, setLeftVal] = useState(1000);
 
-    function scrollRight() {
-        if (containerCurrent) {
-            setLeftVal(leftVal + containerCurrent.clientWidth);
-            containerCurrent.scroll({ top: 0, left: leftVal, behavior: 'smooth' });
+    const latestBlocksContainer: any = useRef(null);
+
+    useEffect(() => {
+        latestBlocksContainer.current.getElement();
+        console.log(latestBlocksContainer);
+    }, []);
+
+    function clickScroll(direction) {
+        if (latestBlocksContainer.current && direction === 'right') {
+            setLeftVal(leftVal + latestBlocksContainer.current.getElement().clientWidth);
+            latestBlocksContainer.current.getElement().scrollTo({ top: 0, left: leftVal, behavior: 'smooth' });
             console.log(leftVal);
         }
     }
 
     return (
         <div className="latestBlocksAll">
-            <div ref={container} className="latestBlocksContainer">
+            <ScrollContainer
+                className="latestBlocksContainer"
+                hideScrollbars={false}
+                vertical={false}
+                ref={latestBlocksContainer}
+            >
                 {latestBlocks.map((block, i) => (
                     <BlockCard key={i} block={block} />
                 ))}
-            </div>
-            <div className="clickScroll" onClick={scrollRight}>
+            </ScrollContainer>
+            <div
+                className="clickScroll"
+                onClick={() => {
+                    clickScroll('right');
+                }}
+            >
                 <RightCaret className="rightCaret" />
                 <RightCaret className="rightCaret small" />
             </div>
