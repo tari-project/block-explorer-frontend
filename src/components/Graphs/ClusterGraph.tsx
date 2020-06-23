67a94f3c379e98a5d0a4846a3eb3cf771a118ed3
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-06-23 13:34:36 +0200

    Cleaned up the function positioning
    Changed timing and removed multiple render artifact

--- src/components/Graphs/ClusterGraph.tsx
+++ src/components/Graphs/ClusterGraph.tsx
@@ -10,6 +10,79 @@ interface Props {
     height: number;
 }
 
+function centroid(nodes) {
+    let x = 0;
+    let y = 0;
+    let z = 0;
+    for (const d of nodes) {
+        const k = d.r ** 2;
+        x += d.x * k;
+        y += d.y * k;
+        z += k;
+    }
+    return { x: x / z, y: y / z };
+}
+
+function forceCluster(nodes) {
+    const strength = 0.2;
+
+    function force(alpha) {
+        const centroids: any = d3Array.rollup(nodes, centroid, (d: any) => d.data.group);
+        const l = alpha * strength;
+        for (const d of nodes) {
+            const { x: cx, y: cy } = centroids.get(d.data.group);
+            d.vx -= (d.x - cx) * l;
+            d.vy -= (d.y - cy) * l;
+        }
+    }
+
+    force.initialize = (_) => (nodes = _);
+
+    return force;
+}
+
+function forceCollide(nodes) {
+    const alpha = 0.4; // fixed for greater rigidity!
+    const padding1 = 2; // separation between same-color nodes
+    const padding2 = 6; // separation between different-color nodes
+    let maxRadius;
+
+    function force() {
+        const quadTree = d3.quadtree(
+            nodes,
+            (d: any) => d.x,
+            (d) => d.y
+        );
+        for (const d of nodes) {
+            const r = d.r + maxRadius;
+            const nx1 = d.x - r,
+                ny1 = d.y - r;
+            const nx2 = d.x + r,
+                ny2 = d.y + r;
+            quadTree.visit((q: any, x1, y1, x2, y2) => {
+                if (!q.length)
+                    do {
+                        if (q.data !== d) {
+                            const r = d.r + q.data.r + (d.data.group === q.data.data.group ? padding1 : padding2);
+                            let x = d.x - q.data.x,
+                                y = d.y - q.data.y,
+                                l = Math.hypot(x, y);
+                            if (l < r) {
+                                l = ((l - r) / l) * alpha;
+                                return (d.x -= x *= l), (d.y -= y *= l), (q.data.x += x), (q.data.y += y);
+                            }
+                        }
+                    } while ((q = q.next));
+                return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+            });
+        }
+    }
+
+    force.initialize = (_) => (maxRadius = Math.max(...nodes.map((o) => o.r)) + Math.max(padding1, padding2));
+
+    return force;
+}
+
 export default function ClusterGraph({ width, height, data }: Props) {
     useEffect(() => {
         const groups = {},
@@ -24,25 +97,23 @@ export default function ClusterGraph({ width, height, data }: Props) {
             }
         });
 
-        for (const property in groups) {
-            if (groups.hasOwnProperty(property)) {
-                const children = {
-                    children: groups[property]
-                };
-                childGroups.children.push(children);
-            }
-        }
+        Object.keys(groups).forEach((property) => {
+            const children = {
+                children: groups[property]
+            };
+            childGroups.children.push(children);
+        });
 
         const pack = () =>
             d3.pack().size([width, height]).padding(1)(d3.hierarchy(childGroups).sum((d: any) => d.size));
-        let nodes: any = pack().leaves();
+        const nodes: any = pack().leaves();
 
         const simulation = d3
             .forceSimulation(nodes)
             .force('x', d3.forceX(width / 2).strength(0.1))
             .force('y', d3.forceY(height / 2).strength(0.1))
-            .force('cluster', forceCluster())
-            .force('collide', forceCollide());
+            .force('cluster', forceCluster(nodes))
+            .force('collide', forceCollide(nodes));
 
         const svg = d3
             .select('#chart')
@@ -94,90 +165,12 @@ export default function ClusterGraph({ width, height, data }: Props) {
             .on('mouseleave', hideTooltip);
 
         node.transition()
-            .delay((d, i) => Math.random() * 500)
-            .duration(750)
-            .attrTween('r', (d: any) => {
-                const i = d3.interpolate(0, d.r);
-                return (t) => (d.r = i(t));
-            });
+            .delay((d, i) => 50)
+            .duration(750);
 
         simulation.on('tick', () => {
             node.attr('cx', (d: any) => d.x).attr('cy', (d: any) => d.y);
         });
-
-        function forceCluster() {
-            const strength = 0.2;
-
-            function force(alpha) {
-                const centroids: any = d3Array.rollup(nodes, centroid, (d: any) => d.data.group);
-                const l = alpha * strength;
-                for (const d of nodes) {
-                    const { x: cx, y: cy } = centroids.get(d.data.group);
-                    d.vx -= (d.x - cx) * l;
-                    d.vy -= (d.y - cy) * l;
-                }
-            }
-
-            force.initialize = (_) => (nodes = _);
-
-            return force;
-        }
-
-        function forceCollide() {
-            const alpha = 0.4; // fixed for greater rigidity!
-            const padding1 = 2; // separation between same-color nodes
-            const padding2 = 6; // separation between different-color nodes
-            let maxRadius;
-
-            function force() {
-                const quadTree = d3.quadtree(
-                    nodes,
-                    (d: any) => d.x,
-                    (d) => d.y
-                );
-                for (const d of nodes) {
-                    const r = d.r + maxRadius;
-                    const nx1 = d.x - r,
-                        ny1 = d.y - r;
-                    const nx2 = d.x + r,
-                        ny2 = d.y + r;
-                    quadTree.visit((q: any, x1, y1, x2, y2) => {
-                        if (!q.length)
-                            do {
-                                if (q.data !== d) {
-                                    const r =
-                                        d.r + q.data.r + (d.data.group === q.data.data.group ? padding1 : padding2);
-                                    let x = d.x - q.data.x,
-                                        y = d.y - q.data.y,
-                                        l = Math.hypot(x, y);
-                                    if (l < r) {
-                                        l = ((l - r) / l) * alpha;
-                                        return ((d.x -= x *= l), (d.y -= y *= l), (q.data.x += x), (q.data.y += y));
-                                    }
-                                }
-                            } while ((q = q.next));
-                        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-                    });
-                }
-            }
-
-            force.initialize = (_) => (maxRadius = Math.max(...nodes.map((o) => o.r)) + Math.max(padding1, padding2));
-
-            return force;
-        }
-
-        function centroid(nodes) {
-            let x = 0;
-            let y = 0;
-            let z = 0;
-            for (const d of nodes) {
-                const k = d.r ** 2;
-                x += d.x * k;
-                y += d.y * k;
-                z += k;
-            }
-            return { x: x / z, y: y / z };
-        }
     }, [data, width, height]);
 
     return <div id="chart" />;
