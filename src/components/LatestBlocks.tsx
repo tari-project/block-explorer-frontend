03877b2925aa879e156f1db2d4377261868f178b
Shan <47271333+shanimal08@users.noreply.github.com>
2020-06-24 12:50:26 +0200

    Click to scroll Latest Blocks (#100)
    * Click scroll WIP
    
    * Click scroll (right) WITH drag scroll
    
    * Click to scroll show/hide carets
    
    * Fixed the hide/show carets
    
    * Used Indiana drag  scroll's onscroll end/start

--- src/components/LatestBlocks.tsx
+++ src/components/LatestBlocks.tsx
@@ -1,5 +1,5 @@
 import BlockCard from './BlockCard';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import { BlocksEntity } from '../types/Blocks';
 import { ReactComponent as RightCaret } from '../assets/right-caret.svg';
@@ -13,15 +13,106 @@ function LatestBlocks({ blocks }: { blocks: BlocksEntity[] }) {
         setLatestBlocks(blocks as BlocksEntity[]);
     }, [blocks]);
 
+    const [leftVal, setLeftVal] = useState(0);
+    const [showLeft, setShowLeft] = useState(false);
+    const [showRight, setShowRight] = useState(true);
+
+    const latestBlocksContainer: any = useRef(null);
+
+    useEffect(() => {
+        latestBlocksContainer.current.getElement();
+    }, []);
+
+    useEffect(() => {
+        if (leftVal > 0) {
+            setShowLeft(true);
+        } else {
+            setShowLeft(false);
+        }
+    }, [leftVal]);
+
+    function clickScroll(direction) {
+        const element = latestBlocksContainer.current.getElement();
+        let leftOffset = leftVal;
+
+        if (latestBlocksContainer.current && direction === 'right') {
+            //get left val for scroll to
+            leftOffset += element.clientWidth;
+            //scroll
+            element.scrollTo({ top: 0, left: leftOffset, behavior: 'smooth' });
+        } else if (latestBlocksContainer.current && direction === 'left') {
+            //get back!
+            leftOffset -= element.clientWidth;
+            //scroll
+            element.scrollTo({ top: 0, left: -leftOffset, behavior: 'smooth' });
+        }
+
+        setLeftVal(leftOffset);
+
+        if (element.scrollWidth - element.scrollLeft <= element.clientWidth) {
+            setShowRight(false);
+        } else {
+            setShowRight(true);
+        }
+    }
+
+    function onStartScroll() {
+        const element = latestBlocksContainer.current.getElement();
+
+        if (element.scrollLeft > 0) {
+            setShowLeft(true);
+        }
+    }
+    function onEndScroll() {
+        const element = latestBlocksContainer.current.getElement();
+
+        if (element.scrollLeft === 0) {
+            setShowLeft(false);
+        }
+
+        if (element.scrollWidth - element.scrollLeft <= element.clientWidth) {
+            setShowRight(false);
+        } else {
+            setShowRight(true);
+        }
+    }
+
     return (
         <div className="latestBlocksAll">
-            <ScrollContainer className="latestBlocksContainer" hideScrollbars={false} vertical={false}>
+            {showLeft && (
+                <div
+                    className="clickScroll left"
+                    onClick={() => {
+                        clickScroll('left');
+                    }}
+                >
+                    <RightCaret className="rightCaret" />
+                    <RightCaret className="rightCaret small" />
+                </div>
+            )}
+            <ScrollContainer
+                className="latestBlocksContainer"
+                hideScrollbars={false}
+                vertical={false}
+                ref={latestBlocksContainer}
+                onStartScroll={onStartScroll}
+                onEndScroll={onEndScroll}
+            >
                 {latestBlocks.map((block, i) => (
                     <BlockCard key={i} block={block} />
                 ))}
             </ScrollContainer>
-            <RightCaret className="rightCaret" />
-            <RightCaret className="rightCaret small" />
+            {showRight && (
+                <div
+                    className="clickScroll"
+                    onClick={() => {
+                        clickScroll('right');
+                    }}
+                >
+                    <RightCaret className="rightCaret" />
+                    <RightCaret className="rightCaret small" />
+                </div>
+            )}
         </div>
     );
 }
